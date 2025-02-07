3a716464ef08024715c9e945bb22cc0e0e8cd859
2pasha <kostishinpavlo24@gmail.com>
2025-02-07 09:40:41 +0200

    changed router type and some fixed for deployment

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import { FlightsPage } from '../pages/FlightsPage';
 import { FlightDetailsPage } from '../pages/FlightDetailsPage';
 import { CartPage } from '../pages/CartPage';
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: '/',
     element: <FlightsPage />,
